29b9a989764bf0895b6bc5fc0cee8290fa8a1393
Shalford25 <spencer.halford@lander.edu>
2025-02-19 11:57:29 -0500

    pain

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
+import {useState, createContext} from "react" //Step 1
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import NavBar from './Components/NavBar';
+import Home from "./Components/Home"
+import {books} from "./Model/books.json"; //Step2
+import Books from './Components/Books';
+import ContactUs from "./Components/ContactUs";
+export const DataContext=createContext(""); //Step3
+export default function App() {
+  const [logStatus,setLogStatus]=useState(0); //Step4
+  const [booklist,setBookList]=useState(books); //Step 5
+  console.log("This got logged");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+    <DataContext.Provider value={{logStatus:logStatus, setLogStatus:setLogStatus,
+      booklist:booklist,setBookList:setBookList}}>
+    <div className='w-[100vw] h-[120vh] bg-amber-100'>
+          <NavBar/>    
+      <div className='text-center'>
+        <BrowserRouter>
+          <Routes>
+          <Route path="/*" element={<Home/>}/>
+          <Route path="/Home" element={<Home/>}/>
+          <Route path="/Books" element={<Books/>}/>
+          <Route path="/ContactUs" element={<ContactUs/>}/>
+          </Routes>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </DataContext.Provider>
+    </div>
+   );
 }
 
-export default App
